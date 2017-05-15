07973b53b5c7c6b8f982694dfd636629d52081e4
Rafael Chacón <rafaelchacon@gmail.com>
2017-05-14 18:35:43 -0700

    Code improvements

--- src/main/scala/org/memcached/types/Lru.scala
+++ src/main/scala/org/memcached/types/Lru.scala
@@ -17,39 +17,56 @@ case class Node[A,B](key: A,
                      var pre: Option[Node[A,B]] = None,
                      var next: Option[Node[A,B]] = None)
 
-case class Lru[A,B](maxCacheSizeBytes: Long, itemMaxSizeInBytes: Long, var currentSize: Long = 0L)(implicit sizer: SizeInBytes[B]) {
+/**
+  * This LRU cache is implemented as LinkedHashMap. The linked list is updated every time there is an operation in the list.
+  * The head of the list is the most recent used item. The tail is the oldest.
+  * This was used as reference on how to implement the LRU cache:
+  * https://alaindefrance.wordpress.com/2014/10/05/lru-cache-implementation/
+  *
+  * @param maxCacheSizeBytes Max value in bytes of this cache
+  * @param itemMaxSizeInBytes Max value that an item could be
+  * @param sizer implicit function that retrieves the size of the value
+  * @tparam A Key Type
+  * @tparam B Value Type
+  */
+case class Lru[A,B](maxCacheSizeBytes: Long, itemMaxSizeInBytes: Long)(implicit sizer: SizeInBytes[B]) {
   var hash: mutable.HashMap[A, Node[A,B]] = new mutable.HashMap()
   var tailOpt:Option[Node[A,B]] = None
   var headOpt:Option[Node[A,B]] = None
+  var currentSize: Long = 0L
 
+  /**
+    * Fetches a key from the LRU
+    * @param key
+    * @return
+    */
   def get(key: A): Option[B] = {
     hash.get(key) match {
       case Some(node) =>
-        remove(node)
-        setHead(node)
+        updateList(node)
         Some(node.value)
       case None =>
         None
     }
   }
 
+  /**
+    * Sets a key in the LRU
+    * @param key to set
+    * @param value to set
+    */
+
   def set(key: A, value: B): Unit = {
-    (hash.get(key), tailOpt) match {
-      case (Some(oldValue), _) if sizer.size(value) <= itemMaxSizeInBytes =>
+    hash.get(key) match {
+      case Some(oldValue) if sizer.size(value) <= itemMaxSizeInBytes =>
         currentSize -= sizer.size(oldValue.value)
         oldValue.value = value
-        remove(oldValue)
-        setHead(oldValue)
+        updateList(oldValue)
         cleanCache(sizer.size(value))
         currentSize += sizer.size(value)
-      case (None, Some(_)) if sizer.size(value) <= itemMaxSizeInBytes =>
+      case None if sizer.size(value) <= itemMaxSizeInBytes =>
         val newNode = Node(key, value)
         cleanCache(sizer.size(value))
-        currentSize += sizer.size(value)
-        setHead(newNode)
-        hash.put(key, newNode)
-      case (None, None) if sizer.size(value) <= itemMaxSizeInBytes =>
-        val newNode = Node(key, value)
         currentSize += sizer.size(value)
         setHead(newNode)
         hash.put(key, newNode)
@@ -57,6 +74,12 @@ case class Lru[A,B](maxCacheSizeBytes: Long, itemMaxSizeInBytes: Long, var curre
         throw new RuntimeException("Attempting to insert an item that is bigger than max allowed size")
     }
   }
+
+  /**
+    * Deletes key from cache. Returns false if the key was not found
+    * @param key
+    * @return
+    */
 
   def delete(key: A): Boolean = {
     hash.get(key) match {
@@ -67,6 +90,11 @@ case class Lru[A,B](maxCacheSizeBytes: Long, itemMaxSizeInBytes: Long, var curre
         true
       case _ => false
     }
+  }
+
+  private def updateList(node: Node[A, B]) = {
+    remove(node)
+    setHead(node)
   }
 
   private def setHead(node: Node[A,B]): Unit = {
